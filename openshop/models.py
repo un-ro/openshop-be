5bb2eff738a72b5fd2070353312f47b233b1e936
Unero <ramadhanbhg@gmail.com>
2025-01-14 14:28:43 +0700

    add model & migration

--- openshop/models.py
+++ openshop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=50)
+    description = models.TextField()
+    shop = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    category = models.CharField(max_length=50)
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    picture = models.CharField(max_length=255)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
