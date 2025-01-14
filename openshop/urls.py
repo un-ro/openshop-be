15ac7ba8cbaba550ab8659344ea60d7bbac26267
Unero <ramadhanbhg@gmail.com>
2025-01-14 18:00:50 +0700

    django without append slash

--- openshop/urls.py
+++ openshop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<uuid:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('products', views.ProductList.as_view(), name='product-list'),
+    path('products/<uuid:pk>', views.ProductDetail.as_view(), name='product-detail'),
 ]
